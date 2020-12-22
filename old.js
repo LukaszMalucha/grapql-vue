acb263810790c1785abf244f006e63c3d80b0224
Lukasz Malucha <lucasmalucha@gmail.com>
2020-12-22 10:56:49 +0000

    data

--- old.js
+++ old.js
@@ -1,11 +1,5 @@
-const  { ApolloServer } = require('apollo-server');
+const  { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
-
-const filePath = path.join(__dirname, 'typeDefs.gql');
-const typeDefs = fs.readFileSync(filePath, 'utf8');
-const resolvers = require('./resolvers');
 
 require('dotenv').config({ path: 'variables.env'});
 const User = require('./models/User');
@@ -16,11 +10,19 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,useUnifiedTopolo
 .then(() => console.log('DB connected'))
 .catch(err => console.error(err));
 
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+  type Query {
+    getTodos: [Todo]
+  }
+`;
 
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
   context: {
     User,
     Post
@@ -31,5 +33,3 @@ server.listen().then(({ url }) => {
   console.log(`Server listen on ${url}`);
 
 });
-
-
