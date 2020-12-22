41c8f7d3a6877c8c567a96e4fd6acad3e52b3104
Lukasz Malucha <lucasmalucha@gmail.com>
2020-12-22 14:22:38 +0000

    vue

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+      <router-view/>
+      <h1>APP</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "App",
+    components: {
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+}
+</script>
--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 
+
+// Import typeDefs and resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf8');
 const resolvers = require('./resolvers');
 
+// Import Env variables and Mongoose models
 require('dotenv').config({ path: 'variables.env'});
 const User = require('./models/User');
 const Post = require('./models/Post');
 
-
+// Connect to DB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,useUnifiedTopology: true } )
 .then(() => console.log('DB connected'))
 .catch(err => console.error(err));
 
 
-
+// Create Apollo GraphQL Server and wire it with typeDefs, resolvers and context objects
 const server = new ApolloServer({
   typeDefs,
   resolvers,
